a772e3263996ee1f22aa586ccb6aaf16c5251aee
erickhansx <erickhansx@gmail.com>
2023-03-29 23:47:09 -0600

    Update app to display routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Fragment } from 'react';
+import About from './components/About/About';
+import Intro from './components/Intro/Intro';
+import DropdownMenu from './components/UI/DropdownMenu';
+import Navbar from './components/UI/Navbar';
+import NavItem from './components/UI/NavItem';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Fragment>
+      <Navbar>
+        <NavItem>
+          <DropdownMenu />
+        </NavItem>
+      </Navbar>
+      <Intro />
+      <About />
+    </Fragment>
+  );
 }
 
 export default App;
