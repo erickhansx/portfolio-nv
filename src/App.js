dd13784dcb8b9fa080c1525a1688cd7cad9d728b
erickhansx <erickhansx@gmail.com>
2023-03-30 23:55:30 -0600

    Update app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Fragment } from 'react';
 import About from './components/About/About';
 import Intro from './components/Intro/Intro';
+import Modal from './components/Projects/Modal';
+import Projects from './components/Projects/Projects';
 import DropdownMenu from './components/UI/DropdownMenu';
 import Navbar from './components/UI/Navbar';
 import NavItem from './components/UI/NavItem';
@@ -15,6 +17,7 @@ function App() {
       </Navbar>
       <Intro />
       <About />
+      <Projects />
     </Fragment>
   );
 }
